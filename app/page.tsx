99b697cc9c14af1354c6f693239aacb46a352cbb
alexandrexavier <jalexandre.xavier@gmail.com>
2025-10-28 19:03:07 +0000

    install shadcn-ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import { ThemeToggle } from "@/components/ui/theme-toggle";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1>Home</h1>
+      <ThemeToggle />
     </div>
   );
 }
